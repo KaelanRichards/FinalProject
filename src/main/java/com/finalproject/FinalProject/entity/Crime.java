3bac86761bacde552773bcd23d96883001932908
michelle <michellehk1993@gmail.com>
2018-09-10 09:29:46 -0400

    edited Crime class getters setters

--- src/main/java/com/finalproject/FinalProject/entity/Crime.java
+++ src/main/java/com/finalproject/FinalProject/entity/Crime.java
@@ -13,131 +13,86 @@ public class Crime {
 	private String address;
 	@JsonProperty("arrest_charge")
 	private String arrestCharge;
-//	@JsonProperty("block_id")
-//	private Integer blockId;
-//	@JsonProperty("charge_description")
-//	private String chargeDesc;
-//	@JsonProperty("council_district")
-//	private Integer councilDis;
-//	@JsonProperty("crime_id")
-//	private Integer crimeId;
-//	@JsonProperty("incident_timestamp")
-//	private String date;
-//	@JsonProperty("neighborhood")
-//	private String neighborhood;
-//	@JsonProperty("scout_car_area")
-//	private Integer scoutCar;
+	@JsonProperty("block_id")
+	private String blockId;
+	@JsonProperty("charge_description")
+	private String chargeDesc;
+	@JsonProperty("council_district")
+	private String councilDis;
+	@JsonProperty("crime_id")
+	private String crimeId;
+	@JsonProperty("incident_timestamp")
+	private String date;
+	@JsonProperty("neighborhood")
+	private String neighborhood;
+	@JsonProperty("scout_car_area")
+	private String scoutCar;
 	
-	
-	//constructor
 	public Crime() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-
 	public String getAddress() {
 		return address;
 	}
-
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
-
 	public String getArrestCharge() {
 		return arrestCharge;
 	}
-
-
 	public void setArrestCharge(String arrestCharge) {
 		this.arrestCharge = arrestCharge;
 	}
-
-
+	public String getBlockId() {
+		return blockId;
+	}
+	public void setBlockId(String blockId) {
+		this.blockId = blockId;
+	}
+	public String getChargeDesc() {
+		return chargeDesc;
+	}
+	public void setChargeDesc(String chargeDesc) {
+		this.chargeDesc = chargeDesc;
+	}
+	public String getCouncilDis() {
+		return councilDis;
+	}
+	public void setCouncilDis(String councilDis) {
+		this.councilDis = councilDis;
+	}
+	public String getCrimeId() {
+		return crimeId;
+	}
+	public void setCrimeId(String crimeId) {
+		this.crimeId = crimeId;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
+	}
+	public String getScoutCar() {
+		return scoutCar;
+	}
+	public void setScoutCar(String scoutCar) {
+		this.scoutCar = scoutCar;
+	}
 	@Override
 	public String toString() {
-		return "Crime [address=" + address + ", arrestCharge=" + arrestCharge + "]";
+		return "Crime [address=" + address + ", arrestCharge=" + arrestCharge + ", blockId=" + blockId + ", chargeDesc="
+				+ chargeDesc + ", councilDis=" + councilDis + ", crimeId=" + crimeId + ", date=" + date
+				+ ", neighborhood=" + neighborhood + ", scoutCar=" + scoutCar + "]";
 	}
-
-
-//	public Integer getBlockId() {
-//		return blockId;
-//	}
-//
-//
-//	public void setBlockId(Integer blockId) {
-//		this.blockId = blockId;
-//	}
-//
-//
-//	public String getChargeDesc() {
-//		return chargeDesc;
-//	}
-//
-//
-//	public void setChargeDesc(String chargeDesc) {
-//		this.chargeDesc = chargeDesc;
-//	}
-//
-//
-//	public Integer getCouncilDis() {
-//		return councilDis;
-//	}
-//
-//
-//	public void setCouncilDis(Integer councilDis) {
-//		this.councilDis = councilDis;
-//	}
-//
-//
-//	public Integer getCrimeId() {
-//		return crimeId;
-//	}
-//
-//
-//	public void setCrimeId(Integer crimeId) {
-//		this.crimeId = crimeId;
-//	}
-//
-//
-//	public String getDate() {
-//		return date;
-//	}
-//
-//
-//	public void setDate(String date) {
-//		this.date = date;
-//	}
-//
-//
-//	public String getNeighborhood() {
-//		return neighborhood;
-//	}
-//
-//
-//	public void setNeighborhood(String neighborhood) {
-//		this.neighborhood = neighborhood;
-//	}
-//
-//
-//	public Integer getScoutCar() {
-//		return scoutCar;
-//	}
-//
-//
-//	public void setScoutCar(Integer scoutCar) {
-//		this.scoutCar = scoutCar;
-//	}
-
-
-//	@Override
-//	public String toString() {
-//		return "address=" + address + ", arrestCharge=" + arrestCharge + ", blockId=" + blockId + ", chargeDesc="
-//				+ chargeDesc + ", councilDis=" + councilDis + ", crimeId=" + crimeId + ", date=" + date
-//				+ ", neighborhood=" + neighborhood + ", scoutCar=" + scoutCar ;
-//	}
+	
 	
 	
 	
