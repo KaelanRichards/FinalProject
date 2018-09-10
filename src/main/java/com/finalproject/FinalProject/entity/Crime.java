d5be6fe0c8f6c17286c2f104e823a7723251b679
jacob <jacob.quickert@gmail.com>
2018-09-10 13:46:28 -0400

    edited Crime entity

--- src/main/java/com/finalproject/FinalProject/entity/Crime.java
+++ src/main/java/com/finalproject/FinalProject/entity/Crime.java
@@ -9,89 +9,168 @@ public class Crime {
 	//these are the data we want to show out from all crime incidents (2016-Present)
 	//https://data.detroitmi.gov/resource/9i6z-cm98.json
 	
-	@JsonProperty("address")
-	private String address;
 	@JsonProperty("arrest_charge")
 	private String arrestCharge;
-	@JsonProperty("block_id")
-	private String blockId;
 	@JsonProperty("charge_description")
 	private String chargeDesc;
-	@JsonProperty("council_district")
-	private String councilDis;
 	@JsonProperty("crime_id")
 	private String crimeId;
+	// data types we are using from the API 
 	@JsonProperty("incident_timestamp")
 	private String date;
 	@JsonProperty("neighborhood")
 	private String neighborhood;
 	@JsonProperty("scout_car_area")
 	private String scoutCar;
+	@JsonProperty("hour_of_day")
+	private int hourOfDay;
+	@JsonProperty("offense_category")
+	private String offenseCategory;
+	@JsonProperty("precinct")
+	private int precinct;
+	@JsonProperty ("year")
+	private int year;
+	@JsonProperty ("longitude")
+	private double longitude;
+	@JsonProperty("latitude")
+	private double latitude;
+	
 	
 	public Crime() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
+
+
 	public String getArrestCharge() {
 		return arrestCharge;
 	}
+
+
 	public void setArrestCharge(String arrestCharge) {
 		this.arrestCharge = arrestCharge;
 	}
-	public String getBlockId() {
-		return blockId;
-	}
-	public void setBlockId(String blockId) {
-		this.blockId = blockId;
-	}
+
+
 	public String getChargeDesc() {
 		return chargeDesc;
 	}
+
+
 	public void setChargeDesc(String chargeDesc) {
 		this.chargeDesc = chargeDesc;
 	}
-	public String getCouncilDis() {
-		return councilDis;
-	}
-	public void setCouncilDis(String councilDis) {
-		this.councilDis = councilDis;
-	}
+
+
 	public String getCrimeId() {
 		return crimeId;
 	}
+
+
 	public void setCrimeId(String crimeId) {
 		this.crimeId = crimeId;
 	}
+
+
 	public String getDate() {
 		return date;
 	}
+
+
 	public void setDate(String date) {
 		this.date = date;
 	}
+
+
 	public String getNeighborhood() {
 		return neighborhood;
 	}
+
+
 	public void setNeighborhood(String neighborhood) {
 		this.neighborhood = neighborhood;
 	}
+
+
 	public String getScoutCar() {
 		return scoutCar;
 	}
+
+
 	public void setScoutCar(String scoutCar) {
 		this.scoutCar = scoutCar;
 	}
+
+
+	public int getHourOfDay() {
+		return hourOfDay;
+	}
+
+
+	public void setHourOfDay(int hourOfDay) {
+		this.hourOfDay = hourOfDay;
+	}
+
+
+	public String getOffenseCategory() {
+		return offenseCategory;
+	}
+
+
+	public void setOffenseCategory(String offenseCategory) {
+		this.offenseCategory = offenseCategory;
+	}
+
+
+	public int getPrecinct() {
+		return precinct;
+	}
+
+
+	public void setPrecinct(int precinct) {
+		this.precinct = precinct;
+	}
+
+
+	public int getYear() {
+		return year;
+	}
+
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+
 	@Override
 	public String toString() {
-		return "Crime [address=" + address + ", arrestCharge=" + arrestCharge + ", blockId=" + blockId + ", chargeDesc="
-				+ chargeDesc + ", councilDis=" + councilDis + ", crimeId=" + crimeId + ", date=" + date
-				+ ", neighborhood=" + neighborhood + ", scoutCar=" + scoutCar + "]";
+		return "Crime [arrestCharge=" + arrestCharge + ", chargeDesc=" + chargeDesc + ", crimeId=" + crimeId + ", date="
+				+ date + ", neighborhood=" + neighborhood + ", scoutCar=" + scoutCar + ", hourOfDay=" + hourOfDay
+				+ ", offenseCategory=" + offenseCategory + ", precinct=" + precinct + ", year=" + year + ", longitude="
+				+ longitude + ", latitude=" + latitude + "]";
 	}
+	
+	
 	
 	
 	
