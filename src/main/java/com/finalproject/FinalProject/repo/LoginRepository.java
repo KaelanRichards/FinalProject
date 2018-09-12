c8503835cf9b013b904f46b5f2a2a98bf9efa16b
michelle <michellehk1993@gmail.com>
2018-09-12 10:47:26 -0400

    login page is now done

--- src/main/java/com/finalproject/FinalProject/repo/LoginRepository.java
+++ src/main/java/com/finalproject/FinalProject/repo/LoginRepository.java
@@ -2,15 +2,15 @@ package com.finalproject.FinalProject.repo;
 
 
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.finalproject.FinalProject.entity.LoginUser;
 
 public interface LoginRepository extends JpaRepository<LoginUser, Long>{
-	List<LoginUser> findByUsername (String username);
-	List<LoginUser> findByPassword (String password);
+	Optional<LoginUser> findByUsername (String username);
+	Optional<LoginUser> findByPassword (String password);
 
 	
 
