f796d32cbdcb1f1583e9610338d520f136780f9a
jacob <jacob.quickert@gmail.com>
2018-09-13 14:17:17 -0400

    refactored the access to our Detroit API

--- src/main/java/com/finalproject/FinalProject/util/CrimeUtility.java
+++ src/main/java/com/finalproject/FinalProject/util/CrimeUtility.java
@@ -13,6 +13,7 @@ import com.finalproject.FinalProject.entity.Crime;
 
 public class CrimeUtility {
 	
+	
 	public static ArrayList<Crime> crimesNearAddress(ArrayList<Crime> crimeList, double userLat, double userLong) {
 		
 		ArrayList<Crime> crimesInRange = new ArrayList<Crime>();
@@ -61,10 +62,7 @@ public class CrimeUtility {
 		for (Crime c : burglary.getBody()) {
 			theftList.add(c);
 		}
-		// test to see if we have the right data
-		for (int i = 0; i < theftList.size(); i++) {
-			System.out.println(theftList.get(i));
-		}
+		
 		theftList = crimesNearAddress(theftList, userLat, userLong);
 		return theftList;
 	}
@@ -134,4 +132,135 @@ public class CrimeUtility {
 		violentList = crimesNearAddress(violentList, userLat, userLong);
 		return violentList;
 	}
+	
+	public static ArrayList<Crime> createMasterList(ArrayList<Crime> theftList, ArrayList<Crime> violentList, ArrayList<Crime> sexList){
+		
+		ArrayList<Crime> masterList = new ArrayList<Crime>();
+		masterList.addAll(theftList);
+		masterList.addAll(violentList);
+		masterList.addAll(sexList);
+		
+		return masterList;
+	}
+	
+	public static int[] getCrimeTotals(String year, double userLat, double userLong) {
+		
+		ArrayList<Crime> theftList = CrimeUtility.theftOffense(year, userLat, userLong);
+		ArrayList<Crime> violentList = CrimeUtility.violentOffense(year, userLat, userLong);
+		ArrayList<Crime> sexList = CrimeUtility.sexOffense(year, userLat, userLong);
+		ArrayList<Crime> masterList = CrimeUtility.createMasterList(theftList, violentList, sexList);
+		int[] totals = {violentList.size(),sexList.size(), theftList.size(),masterList.size()};
+		
+		return totals;
+	}
+	
+	// Calculate the crime data decrease from 2016 to 2018
+	public static int calculateDecrease(int crime2016, int crime2018) {
+		double decrease = crime2016 - crime2018;
+		double decreaseTotal = decrease / crime2016;
+		double decreasePercentage = decreaseTotal * 100;
+
+		return (int) decreasePercentage;
+
+	}
+
+	// Calculate the crime data increase from 2016 to 2018
+	public static int calculateIncrease(int crime2016, int crime2018) {
+
+		double increase = crime2018 - crime2016;
+		double increaseTotal = increase / crime2016;
+		double increasePercentage = increaseTotal * 100;
+		return (int) increasePercentage;
+	}
+	// we pass in a score weight (all weights should sum to 100) and to ints for crime comparison
+	// the return should be one score (of type int) for a particular crime category
+	public static double calculateCrimeScore(double scoreWeight, int crime2016, int crime2018) {
+		double score = 25;
+//		System.out.println("Score: " + score);
+//		System.out.println("scoreweight: " + scoreWeight);
+//		
+//		System.out.println("**" + crime2016);
+//		System.out.println("****" + crime2018);
+		// neighborhood gets full points if crime is zero
+		if (crime2018 == 0) {
+			score = scoreWeight;
+			System.out.println("1");
+			return score;
+			
+		} 
+		// neighborhood gets half points for no increase or decrease in crime
+		else if (crime2016 == crime2018) {
+			score = scoreWeight / 2;
+			System.out.println("2");
+			return score;
+		} 
+		// neighborhood gets more points for greater decrease in crime
+		else if (crime2016 > crime2018) {
+			int decrease = calculateDecrease(crime2016, crime2018);
+			if (decrease >= 50) {
+				score += scoreWeight;
+				System.out.println("3");
+				return score;
+			} else if (decrease >= 25) {
+				score = scoreWeight * .75;
+				System.out.println("4");
+				return score;
+			} else if (decrease >= 1) {
+				score = scoreWeight * .6;
+				System.out.println("5");
+				System.out.println(score);
+				return score;
+			}
+		}
+		// neighborhood gets less points for greater increase in crime
+		else if (crime2016 < crime2018) {
+			int increase = calculateIncrease(crime2016, crime2018);
+			if (increase >= 50) {
+				System.out.println("6");
+				score = 0;
+				return score;
+			} else if (increase >= 25) {
+				score = scoreWeight * .25;
+				System.out.println("7");
+				System.out.println(score);
+				return score;
+			} else if (increase >= 1) {
+				score = scoreWeight * .4;
+				System.out.println("8");
+				System.out.println(score);
+				
+				return score;
+			}
+		}
+
+		return score;
+	}
+	
+	public static String sumScoreCategories(int[] crime2016, int[] crime2018) {
+		
+		double vScore = calculateCrimeScore(20, crime2016[0], crime2018[0]);
+		
+		double sScore = calculateCrimeScore(20, crime2016[1], crime2018[1]);
+		double tScore = calculateCrimeScore(20, crime2016[2], crime2018[2]);
+		double overallScore = calculateCrimeScore(40, crime2016[3], crime2018[3]);
+		double sumScores = vScore + sScore + tScore + overallScore;
+		String grade = "";
+		
+		if (sumScores > 89) {
+			grade = "A";
+		} else if (sumScores > 79) {
+			grade = "B";
+		} else if (sumScores > 69) {
+			grade = "C";
+		} else if (sumScores > 59) {
+			grade = "D";
+		} else {
+			grade = "F";
+		}
+		
+		
+		return grade;
+	}
+
+
 }
