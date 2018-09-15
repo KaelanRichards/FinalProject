dd340c24401324fb6a3989fbdd8dc16899d0a17a
Kaelan <kadokaelan@gmail.com>
2018-09-15 11:42:30 -0400

    Swithced over a method im working on to crime controller inorder to avoid merge conflict

--- src/main/java/com/finalproject/FinalProject/controller/CrimeController.java
+++ src/main/java/com/finalproject/FinalProject/controller/CrimeController.java
@@ -10,10 +10,13 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.finalproject.FinalProject.entity.Crime;
+import com.finalproject.FinalProject.entity.geo.GeoJson;
+import com.finalproject.FinalProject.util.AverageCrimeUtility;
 import com.finalproject.FinalProject.util.CrimeUtility;
 import com.finalproject.FinalProject.util.UtilityClass;
 
@@ -41,7 +44,46 @@ public class CrimeController {
 
 
 
-	
+	//this is the GEOCODE API
+	@RequestMapping("/result")
+	public ModelAndView Geo(@RequestParam("address") String address, @RequestParam("city") String city,
+			@RequestParam("state") String state) {
+		RestTemplate restTemplate = new RestTemplate();
+		GeoJson result = restTemplate.getForObject(
+				"https://maps.googleapis.com/maps/api/geocode/json?address=“+address + city + state+“&key=AIzaSyC4_ZSaexxdhNL2hP_MJ4t4vTRUVpigN1Y",
+				GeoJson.class);
+		// real address :
+		// https://maps.googleapis.com/maps/api/geocode/json?address=1750WoodwardAveDetroitMI&key=AIzaSyC4_ZSaexxdhNL2hP_MJ4t4vTRUVpigN1Y
+		System.out.println(result);
+		
+		Double latitude = result.getResults().get(0).getGeometry().getLocation().getLat();
+		Double longitude = result.getResults().get(0).getGeometry().getLocation().getLng();
+		
+//		int[] totals2016 = CrimeUtility.getCrimeTotals(“2016", latitude, longitude);
+//		int[] totals2017 = CrimeUtility.getCrimeTotals(“2017”, latitude, longitude);
+//		int[] totals2018 = CrimeUtility.getCrimeTotals(“2018", latitude, longitude);
+//		String finalScore = CrimeUtility.sumScoreCategories(totals2016, totals2018);
+		
+		ModelAndView mv = new ModelAndView("results");
+		
+//		mv.addObject(“result”, latitude + ” ” + longitude);
+//		mv.addObject(“grade”, finalScore);
+//		mv.addObject(“scores16", Arrays.toString(totals2016));
+//		mv.addObject(“scores17”, Arrays.toString(totals2017));
+//		mv.addObject(“scores18", Arrays.toString(totals2018));
+		
+		
+		int localAverage = (int) AverageCrimeUtility.averageTotalLocalCrimeList(latitude, longitude);
+		int allAverage = (int) AverageCrimeUtility.averageTotalCrimeList();
+		String percentage = AverageCrimeUtility.calculateSafetyPercentage(allAverage, localAverage);
+		
+		mv.addObject("safetyString", percentage);
+		System.out.println(localAverage +" " + allAverage);
+		
+		return mv;
+		
+		//ORDER IS : json -> results -> geometry -> location -> lat&lng
+	}
 	
 
 	
