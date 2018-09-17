ecf6587e62c616837dcf3cf8dcfaae5376f0ff7e
Kaelan <kadokaelan@gmail.com>
2018-09-17 14:27:22 -0400

    got rid of year when searhing through crime 2016-18 dataset

--- src/main/java/com/finalproject/FinalProject/util/AverageCrimeUtility.java
+++ src/main/java/com/finalproject/FinalProject/util/AverageCrimeUtility.java
@@ -13,7 +13,66 @@ import com.finalproject.FinalProject.entity.Crime;
 
 public class AverageCrimeUtility {
 
-	public static int crimeListByYear(String year){
+//	public static int crimeListByYear(String year){
+//		HttpHeaders headers = new HttpHeaders();
+//		headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+//
+//		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+//
+//		RestTemplate restTemplate = new RestTemplate();
+//
+//		ResponseEntity<Crime[]> stolenVehicle = restTemplate.exchange(
+//				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=24001&year=" + year, HttpMethod.GET, entity,
+//				Crime[].class);
+//
+//		ResponseEntity<Crime[]> larceny = restTemplate.exchange(
+//				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=23007&year=" + year, HttpMethod.GET, entity,
+//				Crime[].class);
+//
+//		ResponseEntity<Crime[]> burglary = restTemplate.exchange(
+//				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=22001&year=" + year, HttpMethod.GET, entity,
+//				Crime[].class);
+//
+//		ResponseEntity<Crime[]> firstDegree = restTemplate.exchange(
+//				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=11001&year=" + year, HttpMethod.GET, entity,
+//				Crime[].class);
+//
+//		ResponseEntity<Crime[]> secondDegree = restTemplate.exchange(
+//				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=11007&year=" + year, HttpMethod.GET, entity,
+//				Crime[].class);
+//
+//		ResponseEntity<Crime[]> aggraAssault = restTemplate.exchange(
+//				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=13002&year=" + year, HttpMethod.GET, entity,
+//				Crime[].class);
+//
+//		ResponseEntity<Crime[]> assault = restTemplate.exchange(
+//				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=13001&year=" + year, HttpMethod.GET, entity,
+//				Crime[].class);
+//
+//		ResponseEntity<Crime[]> robbery = restTemplate.exchange(
+//				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=12000&year=" + year, HttpMethod.GET, entity,
+//				Crime[].class);
+//
+//		int sizeAllCrime = assault.getBody().length 
+//				+ robbery.getBody().length 
+//				+ aggraAssault.getBody().length
+//				+ firstDegree.getBody().length
+//				+ secondDegree.getBody().length
+//				+ burglary.getBody().length
+//				+ larceny.getBody().length
+//				+ stolenVehicle.getBody().length;
+//		
+//		return sizeAllCrime;
+//		
+//	}
+	
+//	public static int allCrimeList() {
+//		int totalCrimeNumber;
+//		totalCrimeNumber = crimeListByYear("2016") + crimeListByYear("2017") + crimeListByYear("2018");
+//		System.out.println(totalCrimeNumber);
+//		return totalCrimeNumber;
+//	}
+	public static int crimeListFromAPI(){
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
 
@@ -22,36 +81,37 @@ public class AverageCrimeUtility {
 		RestTemplate restTemplate = new RestTemplate();
 
 		ResponseEntity<Crime[]> stolenVehicle = restTemplate.exchange(
-				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=24001&year=" + year, HttpMethod.GET, entity,
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=24001", HttpMethod.GET, entity,
 				Crime[].class);
 
 		ResponseEntity<Crime[]> larceny = restTemplate.exchange(
-				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=23007&year=" + year, HttpMethod.GET, entity,
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=23007", HttpMethod.GET, entity,
 				Crime[].class);
 
 		ResponseEntity<Crime[]> burglary = restTemplate.exchange(
-				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=22001&year=" + year, HttpMethod.GET, entity,
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=22001", HttpMethod.GET, entity,
 				Crime[].class);
 
 		ResponseEntity<Crime[]> firstDegree = restTemplate.exchange(
-				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=11001&year=" + year, HttpMethod.GET, entity,
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=11001", HttpMethod.GET, entity,
 				Crime[].class);
 
 		ResponseEntity<Crime[]> secondDegree = restTemplate.exchange(
-				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=11007&year=" + year, HttpMethod.GET, entity,
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=11007", HttpMethod.GET, entity,
 				Crime[].class);
 
 		ResponseEntity<Crime[]> aggraAssault = restTemplate.exchange(
-				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=13002&year=" + year, HttpMethod.GET, entity,
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=13002", HttpMethod.GET, entity,
 				Crime[].class);
 
 		ResponseEntity<Crime[]> assault = restTemplate.exchange(
-				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=13001&year=" + year, HttpMethod.GET, entity,
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=13001", HttpMethod.GET, entity,
 				Crime[].class);
 
 		ResponseEntity<Crime[]> robbery = restTemplate.exchange(
-				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=12000&year=" + year, HttpMethod.GET, entity,
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=12000", HttpMethod.GET, entity,
 				Crime[].class);
+	
 
 		int sizeAllCrime = assault.getBody().length 
 				+ robbery.getBody().length 
@@ -66,36 +126,106 @@ public class AverageCrimeUtility {
 		
 	}
 	
-	public static int allCrimeList() {
-		int totalCrimeNumber;
-		totalCrimeNumber = crimeListByYear("2016") + crimeListByYear("2017") + crimeListByYear("2018");
-		System.out.println(totalCrimeNumber);
-		return totalCrimeNumber;
-	}
+//	public static int allCrimeList() {
+//		int totalCrimeNumber;
+//		totalCrimeNumber = crimeListFromAPI();
+//		System.out.println(totalCrimeNumber);
+//		return totalCrimeNumber;
+//	}
 	
 	public static double averageTotalCrimeList() {
 
-		double totalCrimeNumber = allCrimeList();
-		double findDetroitAverage = totalCrimeNumber / 3;
+		double totalCrimeNumber = crimeListFromAPI();
+		double findDetroitAverage = totalCrimeNumber;
 		double quarterMileRadiusAverage = findDetroitAverage /  715;
 		System.out.println("Average1: " + findDetroitAverage + " Average2: " + quarterMileRadiusAverage);
 		return quarterMileRadiusAverage;
 	}
 	// this is the avg in 0.25mi^2
 	//real number should be 7.3
-	
-	public static int allLocalCrimeList(double userLat, double userLong) {
-		int var16 = CrimeUtility.getCrimeTotals("2016", userLat, userLong)[3];
-		int var17 = CrimeUtility.getCrimeTotals("2017", userLat, userLong)[3];
-		int var18 = CrimeUtility.getCrimeTotals("2018", userLat, userLong)[3];
-		int total = var16 + var17 + var18;
-		System.out.println("16: " + var16 + " 17: " + var17 + " 18: " + var18 + " total: " + total);
-		return total;
-		
+	public static int crimeLocalList(double userLat, double userLong){
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+		RestTemplate restTemplate = new RestTemplate();
+		ArrayList<Crime> localList = new ArrayList<>();
+
+		ResponseEntity<Crime[]> stolenVehicle = restTemplate.exchange(
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=24001", HttpMethod.GET, entity,
+				Crime[].class);
+
+		ResponseEntity<Crime[]> larceny = restTemplate.exchange(
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=23007", HttpMethod.GET, entity,
+				Crime[].class);
+
+		ResponseEntity<Crime[]> burglary = restTemplate.exchange(
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=22001", HttpMethod.GET, entity,
+				Crime[].class);
+
+		ResponseEntity<Crime[]> firstDegree = restTemplate.exchange(
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=11001", HttpMethod.GET, entity,
+				Crime[].class);
+
+		ResponseEntity<Crime[]> secondDegree = restTemplate.exchange(
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=11007", HttpMethod.GET, entity,
+				Crime[].class);
+
+		ResponseEntity<Crime[]> aggraAssault = restTemplate.exchange(
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=13002", HttpMethod.GET, entity,
+				Crime[].class);
+
+		ResponseEntity<Crime[]> assault = restTemplate.exchange(
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=13001", HttpMethod.GET, entity,
+				Crime[].class);
+
+		ResponseEntity<Crime[]> robbery = restTemplate.exchange(
+				"https://data.detroitmi.gov/resource/9i6z-cm98.json?arrest_charge=12000", HttpMethod.GET, entity,
+				Crime[].class);
+		for (Crime c : assault.getBody()) {
+			localList.add(c);
+		}
+		// adding second degree sex offense to SA list
+		for (Crime c : robbery.getBody()) {
+			localList.add(c);
+		}
+		for (Crime c : aggraAssault.getBody()) {
+			localList.add(c);
+		}
+		for (Crime c : firstDegree.getBody()) {
+			localList.add(c);
+		}
+		// adding second degree sex offense to SA list
+		for (Crime c : secondDegree.getBody()) {
+			localList.add(c);
+		}
+		for (Crime c : stolenVehicle.getBody()) {
+			localList.add(c);
+		}
+		// adding larceny to our theft category list
+		for (Crime c : larceny.getBody()) {
+			localList.add(c);
+		}
+		// adding burglary to our theft category list
+		for (Crime c : burglary.getBody()) {
+			localList.add(c);
+		}
+		localList = CrimeUtility.crimesNearAddress(localList, userLat, userLong);
+		return localList.size();
 	}
+//	public static int allLocalCrimeList(double userLat, double userLong) {
+//		int var16 = CrimeUtility.getCrimeTotals("2016", userLat, userLong)[3];
+//		int var17 = CrimeUtility.getCrimeTotals("2017", userLat, userLong)[3];
+//		int var18 = CrimeUtility.getCrimeTotals("2018", userLat, userLong)[3];
+//		int total = var16 + var17 + var18;
+//		System.out.println("16: " + var16 + " 17: " + var17 + " 18: " + var18 + " total: " + total);
+//		return total;
+//		
+//	}
 	public static double averageTotalLocalCrimeList(double userLat, double userLong) {
 
-		double totalLocalCrimeNumber = allLocalCrimeList(userLat, userLong);
+		double totalLocalCrimeNumber = crimeLocalList(userLat, userLong);
 		double average = totalLocalCrimeNumber / 3;
 		return average;
 	}
